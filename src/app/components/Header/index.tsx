714f49a9acdf6d6f53cd393135f6c7943617eaf5
Andre <andre.torabpour@gmail.com>
2025-08-13 22:47:46 +0200

    fixed design

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -5,13 +5,16 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-white text-gray-200 shadow-lg">
+    
+    <header className="sticky top-4 z-50 bg-white text-gray-200 shadow-lg h-18 border-b-2 border-red-500 shadow">
       <nav className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <Link href="/" className="text-2xl font-bold hover:text-white">
-            <Image src="/images/logo.png" alt="Logo" width={75} height={75}/>
+          <div className="h-8">
+            <Image src="/images/logo.png" alt="Logo" width={100} height={100} className="-mt-12 text-bold object-contain"/>
+          </div>
           </Link>
-
+          
           <div className="flex items-center space-x-6">
               <SideBar/>
           </div>
