018752656b1333fddd1b8ad67949bd2f5d238f29
Andre <andre.torabpour@gmail.com>
2025-08-15 17:37:48 +0200

    testheader fixed

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -1,42 +1,58 @@
-import { render, screen } from '@testing-library/react'
-import Header from '@/app/components/Header'
+import { render, screen } from '@testing-library/react'; //André
+import Header from '@/app/components/Header';
 
 jest.mock('next/image', () => (props: any) => {
   const { priority, ...rest } = props
   return <img {...rest} />
-})
+});
 
 jest.mock('next/link', () => {
   return ({ href, children, ...rest }: any) => (
     <a href={typeof href === 'string' ? href : '#'} {...rest}>
       {children}
     </a>
-  )
-})
+  );
+});
 
-jest.mock('@/app/components/SideBar', () => () => <div>Mock Sidebar</div>)
+jest.mock('@/app/components/SideBar', () => () => <div>Mock Sidebar</div>);
+
 
 describe('Header', () => {
-  it('visar loggan i headern', () => {
-    render(<Header />)
-
-    
-    const logo = screen.getByRole('img', { name: /logo/i })
-    expect(logo).toBeInTheDocument()
-  })
-
-  it('loggan länkar till startsidan', () => {
-    render(<Header />)
-
   
-    const logoLink = screen.getByRole('link', { name: /logo/i })
-    expect(logoLink).toHaveAttribute('href', '/')
-  })
+  describe('Logotypen', () => {
+    it('visar logotypen i headern', () => {
+      render(<Header />);
 
-  it('renderar navigationen', () => {
-    render(<Header />)
+      
+      const logo = screen.getByRole('img', { name: /logo/i });
+      expect(logo).toBeInTheDocument();
+    });
 
+    it('logotypen länkar till startsidan', () => {
+      render(<Header />);
 
-    expect(screen.getByRole('navigation')).toBeInTheDocument()
-  })
-})
+      
+      const logoLink = screen.getByRole('link', { name: /logo/i });
+      expect(logoLink).toHaveAttribute('href', '/');
+    });
+  });
+  
+  
+  describe('Navigationen', () => {
+    it('renderar navigationen', () => {
+      render(<Header />);
+      
+    
+      const navigation = screen.getByRole('navigation');
+      expect(navigation).toBeInTheDocument();
+    });
+    
+    it('renderar sidofältskomponenten (SideBar)', () => {
+      render(<Header />);
+
+      
+      const sidebar = screen.getByText('Mock Sidebar');
+      expect(sidebar).toBeInTheDocument();
+    });
+  });
+});
