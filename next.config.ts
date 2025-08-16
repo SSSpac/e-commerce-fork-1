5559bda3774ccc99dd6ecf014f7218ee4f06ec83
Andre <andre.torabpour@gmail.com>
2025-08-16 22:43:06 +0200

    fixed nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
 
 export default nextConfig;
